d9cbfadcb131b5b83df63616de61a34fb283c61d
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-16 13:36:26 -0400

    criando divs color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+
+function createDivs() {
+  const div = document.createElement('div');
+  document.querySelector('main').appendChild(div);
+  div.setAttribute('id', 'colorArea');   
+  for (let i = 1; i <= 12; i += 1) {
+    const createDiv = document.createElement('div');
+    document.querySelector('#colorArea').appendChild(createDiv);
+    createDiv.setAttribute('id', `color${[i]}`)
+    createDiv.setAttribute('class', 'color')
+  }
+}
+
 function createMain() {
   document.body.appendChild(document.createElement('main'));
   const createCanvas = document.createElement('canvas');
   document.querySelector('main').appendChild(createCanvas);
   createCanvas.setAttribute('id', 'display');
+  createDivs()
 }
 
 function createHeader() {
